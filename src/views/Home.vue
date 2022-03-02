427f83e736051e78cc8afbc5a1cd62f1aa91013c
jinweihuang <ss610349@gmail.com>
2022-03-02 15:06:00 +0800

    tttttt

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import { Indicator } from "mint-ui";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  mounted() {
+    Indicator.open({
+      text: "載入中...",
+      spinnerType: "fading-circle",
+    });
+
+    setTimeout(() => {
+      Indicator.close();
+    }, 2000);
+  },
+};
 </script>
